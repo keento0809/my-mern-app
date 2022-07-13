e129c9db2d1a098918a287dd15ea4bb035b6dbe2
KEENTO <handball0321@gmail.com>
2022-07-13 14:14:13 -0700

    Add forms for login and signup

--- frontend/src/components/form/SignupForm.js
+++ frontend/src/components/form/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FormControl, FormLabel, Input, Box, Button } from "@chakra-ui/react";
 
-const LoginForm = () => {
+const SignupForm = () => {
   const [formInput, setFormInput] = useState({
     email: "",
     password: "",
@@ -9,14 +9,14 @@ const LoginForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("aaa");
+    console.log("bbb");
   };
 
   return (
     <Box pt={6}>
       <form onSubmit={handleSubmit}>
         <FormControl>
-          <FormLabel htmlFor="email" pt={4}>
+          <FormLabel htmlFor="emailForSignup" pt={4}>
             Email
           </FormLabel>
           <Input
@@ -26,13 +26,13 @@ const LoginForm = () => {
             focusBorderColor="pink.100"
             value={formInput.email}
             //   onChange={handleChange}
-            id="email"
+            id="emailForSignup"
             type="email"
             placeholder="Enter email"
           />
         </FormControl>
         <FormControl>
-          <FormLabel htmlFor="password" pt={4}>
+          <FormLabel htmlFor="passwordForSignup" pt={4}>
             Password
           </FormLabel>
           <Input
@@ -42,7 +42,23 @@ const LoginForm = () => {
             focusBorderColor="pink.100"
             value={formInput.password}
             //   onChange={handleChange}
-            id="password"
+            id="passwordForSignup"
+            type="password"
+            placeholder="Enter password"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="password" pt={4}>
+            Password Confirmation
+          </FormLabel>
+          <Input
+            name="passwordConfirmation"
+            px={4}
+            py={1}
+            focusBorderColor="pink.100"
+            value={formInput.password}
+            //   onChange={handleChange}
+            id="passwordConfirmation"
             type="password"
             placeholder="Enter password"
           />
@@ -55,11 +71,11 @@ const LoginForm = () => {
           backgroundColor="pink.100"
           variant="solid"
         >
-          Login
+          Signup
         </Button>
       </form>
     </Box>
   );
 };
 
-export default LoginForm;
+export default SignupForm;
---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Layout from "../Layout/Layout";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Text,
+} from "@chakra-ui/react";
+import LoginForm from "../components/form/LoginForm";
+import SignupForm from "../components/form/SignupForm";
+
+const Auth = () => {
+  return (
+    <Layout>
+      <Tabs isFitted variant="enclosed" pt={6}>
+        <TabList mb="1em">
+          <Tab>Login</Tab>
+          <Tab>Signup</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Text fontSize="2xl" textAlign="center">
+              Login
+            </Text>
+            <LoginForm />
+          </TabPanel>
+          <TabPanel>
+            <Text fontSize="2xl" textAlign="center">
+              Signup
+            </Text>
+            <SignupForm />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Layout>
+  );
+};
+
+export default Auth;
