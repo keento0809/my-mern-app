5e4eeb3838e6d175cded1f2a9b56dc29280f2e73
KEENTO <handball0321@gmail.com>
2022-07-17 12:19:16 -0700

    Add username in userSchema

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -13,6 +13,7 @@ export const heroImg =
   "https://images.unsplash.com/photo-1584473457406-6240486418e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdyb2Nlcnl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60";
 
 export const guestUser = {
+  username: process.env.REACT_APP_GUEST_USER_NAME,
   email: process.env.REACT_APP_GUEST_USER_EMAIL,
   password: process.env.REACT_APP_GUEST_USER_PASSWORD,
 };
