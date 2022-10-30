6912eaaa87eeebf9b693140181ace22b6ade31c0
KEENTO <handball0321@gmail.com>
2022-10-30 16:06:59 -0700

    Deleted unnecessary folder

--- backend/controllers/AuthControllers.js
+++ backend/controllers/AuthControllers.js
@@ -1,20 +1,15 @@
 const { validationResult } = require("express-validator");
 const User = require("../models/UserModels");
-const users = require("../db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  console.log("Logging in");
-
   const loginUser = await User.findOne({ email });
   const hashComparison = await bcrypt.compare(password, loginUser.password);
-
   const payload = { email };
   const option = { expiresIn: 3600000 };
   const newToken = await jwt.sign(payload, process.env.SECRET_KEY, option);
-
   if (loginUser && hashComparison) {
     res.json({
       _id: loginUser.id,
@@ -29,40 +24,31 @@ const login = async (req, res) => {
 
 const signup = async (req, res) => {
   const { username, email, password } = req.body;
-
-  // validate errors
-  // utilize validationResult
   const result = validationResult(req);
   const hasErrors = !result.isEmpty();
   if (hasErrors) {
     return res.status(400).json({ errors: errors.array() });
   }
-
   // check existing users
   const existingUser = await User.findOne({ email });
-
   if (existingUser) {
     return res.status(400).json({
       error: "This user has already existed.",
     });
   }
-
   // hash password
   const salt = await bcrypt.genSalt(10);
   const hashed = await bcrypt.hash(password, salt);
-
   // issue token
   const payload = { email };
   const option = { expiresIn: 3600000 };
   const newToken = await jwt.sign(payload, process.env.SECRET_KEY, option);
-
   // Add new user to DB
   const newUser = await User.create({
     username,
     email,
     password: hashed,
   });
-
   if (newUser) {
     res.status(200).json({
       _id: newUser.id,
@@ -70,7 +56,6 @@ const signup = async (req, res) => {
       email: newUser.email,
       token: newToken,
     });
-    users.push(newUser);
   } else {
     res.status(400);
     throw new Error("Invalid user data");
--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const users = [
-  {
-    email: "testUser@hotmail.com",
-    password: "aaBb1122c",
-  },
-];
-
-module.exports = users;
